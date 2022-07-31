2d2ab4a936fedc29e8de3b2698b82717e50df401
Vitor Buxbaum <vitor.buxbaum@gmail.com>
2022-07-31 20:43:12 -0300

    improves imports

--- image_processing.py
+++ image_processing.py
@@ -1,14 +1,13 @@
 from face_detection import FaceDetector
 from profile_handler import get_image_url
 from time import sleep
-from requests import get
-from PIL import Image
+import requests
 
 
 def has_photo(selector, username):
     image_url = get_image_url(selector)
     sleep(2)
-    img_data = get(image_url).content
+    img_data = requests.get(image_url).content
     with open("photos/" + username + "_image.jpg", "wb") as handler:
         handler.write(img_data)
     return FaceDetector.find_faces("photos/" + username + "_image.jpg")
